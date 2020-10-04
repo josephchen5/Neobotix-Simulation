a59294daf3ef73a798e81564683a99d89b0503ba
josephchen5 <joseph.chen4210@gmail.com>
2020-10-04 11:46:34 +0800

    test

---
+++ neobotix_ws/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/josep/Neobotix-Simulation/neobotix_ws/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/josep/smach_tutorials/smach_ws/devel;/home/josep/catkin_ws/devel;/opt/ros/melodic'.split(';') if '/home/josep/smach_tutorials/smach_ws/devel;/home/josep/catkin_ws/devel;/opt/ros/melodic' != '' else []
