8050ef1592e048fb9adce8db4bd011c226c1b45c
josephchen5 <joseph.chen4210@gmail.com>
2020-11-16 09:16:37 +0800

    update

--- neobotix_ws/build/catkin_generated/order_packages.py
+++
@@ -1,5 +0,0 @@
-# generated from catkin/cmake/template/order_packages.context.py.in
-source_root_dir = '/home/josep/Neobotix-Simulation/neobotix_ws/src'
-whitelisted_packages = ''.split(';') if '' != '' else []
-blacklisted_packages = ''.split(';') if '' != '' else []
-underlay_workspaces = '/home/josep/ros_action_tutorials/catkin_ws/devel;/home/josep/ros_service_tutorial/catkin_ws/devel;/home/josep/Neobotix-Simulation/neobotix_ws/devel;/home/josep/smach_tutorials/smach_ws/devel;/home/josep/catkin_ws/devel;/opt/ros/melodic'.split(';') if '/home/josep/ros_action_tutorials/catkin_ws/devel;/home/josep/ros_service_tutorial/catkin_ws/devel;/home/josep/Neobotix-Simulation/neobotix_ws/devel;/home/josep/smach_tutorials/smach_ws/devel;/home/josep/catkin_ws/devel;/opt/ros/melodic' != '' else []
